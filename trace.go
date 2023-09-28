aa0abdb9f4bdbac4a2ce07686f37a841f35db140
librescan-ci <ci@librescan.org>
2023-09-28 23:32:51 +0000

    fix: missing import

--- trace.go
+++ trace.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	storage "github.com/librescan-org/backend-db"
 )
 
 type TraceType = string
